7238558258a74c28e78d708726786e9ff5286f61
David Gero <dwgero@gmail.com>
2026-08-03 23:25:10 -0400

    Release version 2.2.0

--- crepl.cpp
+++ crepl.cpp
@@ -53,7 +53,7 @@
 
 namespace {
 
-constexpr std::string_view crepl_version = "2.1.7";
+constexpr std::string_view crepl_version = "2.2.0";
 
 void print_crepl_banner(std::ostream& output) {
     output << "Combinator Read-Eval-Print Loop, version "
