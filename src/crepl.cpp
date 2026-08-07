c60745b48d7add241b7eb0269828c58d0d0aba0a
David Gero <dwgero@gmail.com>
2026-08-07 08:29:48 -0400

    Update version to 2.5.2

--- src/crepl.cpp
+++ src/crepl.cpp
@@ -59,7 +59,7 @@
 
 namespace {
 
-constexpr std::string_view crepl_version = "2.5.1";
+constexpr std::string_view crepl_version = "2.5.2";
 
 [[nodiscard]] bool stream_is_terminal(std::FILE* stream) noexcept {
 #if defined(_WIN32)
